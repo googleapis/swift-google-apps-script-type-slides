7dcef23d92840641203d6395c6e1d61007a8c87b
Jeff Ching <chingor@google.com>
2026-09-16 15:23:05 +0000

    feat(generated): update generator with support for unknown keys (#904)
    Adds support for unknown keys
    
    Fixes #447

--- Sources/GoogleAppsScriptTypeSlides/SlidesExtensionPoint.swift
+++ Sources/GoogleAppsScriptTypeSlides/SlidesExtensionPoint.swift
@@ -24,6 +24,8 @@ public struct SlidesExtensionPoint: Codable, Equatable, GoogleCloudWKT._AnyPacka
   /// Required. The endpoint to execute when this extension point is activated.
   public var runFunction: Swift.String = Swift.String()
 
+  @_spi(GoogleCloudInternal) public var _unknownFields: GoogleCloudWKT._UnknownFields = .init()
+
   /// Initialize a new instance of `SlidesExtensionPoint`.
   public init() {}
 
@@ -38,6 +40,38 @@ public struct SlidesExtensionPoint: Codable, Equatable, GoogleCloudWKT._AnyPacka
     var copy = self
     try config(&copy)
     return copy
+  }
+
+  private struct CodingKeys: CodingKey {
+    var stringValue: Swift.String
+    var intValue: Swift.Int? { nil }
+    init(stringValue: Swift.String) { self.stringValue = stringValue }
+    init?(intValue: Swift.Int) { nil }
+
+    static let runFunction = CodingKeys(stringValue: "runFunction")
+
+    static let _knownKeys: Set<Swift.String> = [
+      "runFunction"
+    ]
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    if let value = try container.decodeIfPresent(Swift.String.self, forKey: .runFunction) {
+      self.runFunction = value
+    }
+    for key in container.allKeys where !CodingKeys._knownKeys.contains(key.stringValue) {
+      self._unknownFields.json[key.stringValue] = try container.decode(
+        GoogleCloudWKT.Value.self, forKey: key)
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(self.runFunction, forKey: .runFunction)
+    for (key, value) in self._unknownFields.json {
+      try container.encode(value, forKey: CodingKeys(stringValue: key))
+    }
   }
 
   public static var _anyTypeUrl: Swift.String {
